da4fbbc458e131e440252ad939ce37c6c6d0f0e2
Marie Jakob <54738849+marie-jakob@users.noreply.github.com>
2025-05-06 11:01:04 +0100

    Remove build tools check and add tests for G2

--- tests/testthat/test-ppp.R
+++ tests/testthat/test-ppp.R
@@ -8,8 +8,6 @@ test_that("ppp() gives similar X2 and G2 statistics as TreeBUGS", {
   library("tidyr")
   library("TreeBUGS")
 
-  options(buildtools.check = function(action) FALSE )
-
   skk_red <- subset(skk13, (race == "german"))
   # Fit mptstan() model
   options(mc.cores = parallel::detectCores())
@@ -51,8 +49,11 @@ new new_new (1-Dn)*(1-g1)*(1-g2)"
   # Compare real frequencies from data and T1 function
   expect_equal(as.numeric(t_mptstan$n_real), as.numeric(colSums(skk_tb[3:8])))
   expect_equal(as.numeric(sort(t_mptstan$n_real)), as.numeric(sort(colSums(t_tb$freq.obs))))
+  expect_equal(as.numeric(t_mptstan_g2$n_real), as.numeric(colSums(skk_tb[3:8])))
+  expect_equal(as.numeric(sort(t_mptstan_g2$n_real)), as.numeric(sort(colSums(t_tb_g2$freq.obs))))
 
   # Compare p values (with tolerance)
   expect_equal(t_tb$T1.p, t_mptstan$t1_p.value, tolerance = 1e-1)
+  expect_equal(t_tb_g2$T1.p, t_mptstan_g2$t1_p.value, tolerance = 1e-1)
 
 })
