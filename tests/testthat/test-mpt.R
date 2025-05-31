4ebe173f0dd63e4bb85424b12f7a16a7ef5766bb
Marie Jakob <mjakob@cs.uni-freiburg.de>
2025-05-31 09:19:29 +0100

    adapt fit calls to test environment

--- tests/testthat/test-mpt.R
+++ tests/testthat/test-mpt.R
@@ -1,48 +1,60 @@
 
 test_that("mpt() leads to similar results regardless of aggregation and probability / log probability scale", {
-  skip_if_not_installed("tidyverse")
-  library(tidyverse)
+  skip_if_not_installed("dplyr")
+  skip_if_not_installed("tidyr")
+  library(dplyr)
+  library(tidyr)
 
   skk13 <- mptstan::skk13
   # 1. Non-aggregated Data - prob scale
-  options(mc.cores = parallel::detectCores())
-  u2htsm_model <- make_mpt("inst/extdata/u2htm.eqn")
+  options(mc.cores = 2)
+  u2htsm_model <- suppressWarnings(make_mpt(system.file("extdata", "u2htm.eqn",
+                                                        package = "mptstan")))
   u2htm_formula <- mpt_formula(resp ~ 1 + (1 | id),
                                model = u2htsm_model,
                                data_format = "long")
-  fit_skk <- mpt(u2htm_formula, data = skk13 %>% dplyr::filter(race == "german"),
-                   tree = "type",
-                   init_r = 0.5,
-                 log_p = FALSE
+  fit_skk <- mpt(u2htm_formula, data = skk13 |>
+                   dplyr::filter(race == "german"),
+                 tree = "type",
+                 init_r = 0.5,
+                 log_p = FALSE,
+                 chains = 2,
+                 refresh = 0,
+                 open_progress = FALSE
   )
 
   # 2. Aggregated Data - prob scale
-  skkagg <- skk13 %>%
-    filter(race == "german") %>%
-    count(resp, id, type) %>%
-    pivot_wider(names_from = resp, values_from = n)
-  skkagg[is.na(skkagg)] <- 0
+  skkagg <- skk13 |>
+    dplyr::filter(race == "german") |>
+    dplyr::count(resp, id, type) |>
+    tidyr::pivot_wider(names_from = resp, values_from = n, values_fill = 0)
 
   u2htm_formula_agg <- mpt_formula(~ 1 + (1 | id),
                                model = u2htsm_model, data_format = "wide")
 
   fit_skk_agg <- mpt(u2htm_formula_agg, data = skkagg,
                  tree = "type",
-                 init_r = 0.5
+                 init_r = 0.5,
+                 chains = 2,
+                 refresh = 0, open_progress = FALSE
   )
 
   # 3. Aggregated data - log scale
   fit_skk_log <- mpt(u2htm_formula, data = skk13 %>% dplyr::filter(race == "german"),
                  tree = "type",
                  init_r = 0.5,
-                 log_p = TRUE
+                 log_p = TRUE,
+                 chains = 2,
+                 refresh = 0, open_progress = FALSE
   )
 
   # 4. Aggregated Data - log scale
   fit_skk_agg_log <- mpt(u2htm_formula_agg, data = skkagg,
                      tree = "type",
                      init_r = 0.5,
-                     log_p = TRUE
+                     log_p = TRUE,
+                     chains = 2,
+                     refresh = 0, open_progress = FALSE
   )
 
   expect_equal(fixef(fit_skk)[, 1], fixef(fit_skk_agg)[, 1], tolerance = 1e-1)
@@ -58,59 +70,4 @@ test_that("mpt() leads to similar results regardless of aggregation and probabil
   expect_equal(ranef(fit_skk)$id[, 1, ], ranef(fit_skk_log)$id[, 1, ], tolerance = 1e-1)
   expect_equal(ranef(fit_skk)$id[, 1, ], ranef(fit_skk_agg_log)$id[, 1, ], tolerance = 1e-1)
 
-})
-
-
-test_that("mpt() works the same for a given mpt_formula and a formula", {
-  skk13 <- mptstan::skk13
-  # 1. Non-aggregated Data - prob scale
-  options(mc.cores = parallel::detectCores())
-  u2htsm_model <- make_mpt("inst/extdata/u2htm.eqn")
-  u2htm_formula <- mpt_formula(resp ~ 1 + (1 | id),
-                               model = u2htsm_model,
-                               data_format = "long")
-  fit_skk <- mpt(u2htm_formula, data = skk13 %>% dplyr::filter(race == "german"),
-                 tree = "type",
-                 init_r = 0.5, seed = 2
-  )
-
-  fit_skk_2 <- mpt(resp ~ 1 + (1 | id), model = u2htsm_model,
-                   data_format = "long", tree = "type",
-                   data = skk13,
-                   init_r = 0.5, seed = 2)
-
-  # 2. Aggregated data
-  skkagg <- skk13 %>%
-    filter(race == "german") %>%
-    count(resp, id, type) %>%
-    pivot_wider(names_from = resp, values_from = n)
-  skkagg[is.na(skkagg)] <- 0
-
-  u2htm_formula <- mpt_formula(resp ~ 1 + (1 | id),
-                               model = u2htsm_model,
-                               data_format = "wide")
-
-  fit_skk <- mpt(u2htm_formula, data = skk13agg,
-                 tree = "type",
-                 init_r = 0.5,
-                 seed = 2
-  )
-
-  fit_skk_2 <- mpt(resp ~ 1 + (1 | id), model = u2htsm_model,
-                   data_format = "long", tree = "type",
-                   data = skk13,
-                   init_r = 0.5,
-                   seed = 2)
-  expect_equal(fixef(fit_skk)[, 1], fixef(fit_skk_agg)[, 1], tolerance = 1e-1)
-  expect_equal(fixef(fit_skk)[, 2], fixef(fit_skk_agg)[, 2], tolerance = 1e-1)
-
-  expect_equal(fixef(fit_skk)[, 1], fixef(fit_skk_log)[, 1], tolerance = 1e-1)
-  expect_equal(fixef(fit_skk)[, 2], fixef(fit_skk_log)[, 2], tolerance = 1e-1)
-
-  expect_equal(fixef(fit_skk)[, 1], fixef(fit_skk_agg_log)[, 1], tolerance = 1e-1)
-  expect_equal(fixef(fit_skk)[, 2], fixef(fit_skk_agg_log)[, 2], tolerance = 1e-1)
-
-  expect_equal(ranef(fit_skk)$id[, 1, ], ranef(fit_skk_agg)$id[, 1, ], tolerance = 1e-1)
-  expect_equal(ranef(fit_skk)$id[, 1, ], ranef(fit_skk_log)$id[, 1, ], tolerance = 1e-1)
-  expect_equal(ranef(fit_skk)$id[, 1, ], ranef(fit_skk_agg_log)$id[, 1, ], tolerance = 1e-1)
 })
